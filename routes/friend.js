8e75e08fc5b9ffab9000c2eb3ebfb354f68665d6
akat32 <baduk32127@gmail.com>
2018-06-05 09:26:57 +0900

    add Del friend

--- routes/friend.js
+++ routes/friend.js
@@ -4,10 +4,8 @@ module.exports = friend;
 function friend(app, Users){
   app.post('/add', async(req,res)=>{
     var result1 = await Users.findOne({email : req.body.email});
-    console.log(result1)
     if(!result1) return res.status(404).json({message : "User Not Found!"})
     var result2 = await Users.findOne({token : req.body.token})
-    console.log(result2)
     if(!result2) return res.status(404).json({message : "User Not Found!"})
     var friend = {
       name : result1.name,
@@ -23,15 +21,19 @@ function friend(app, Users){
       {"token" : req.body.token},
       {$push : {friendList : friend}}
     )
-    if(!result3.ok) return res.status(401).json({message : "ERR!"})
+    if(!result3.ok) return res.status(500).json({message : "ERR!"})
     var result4 = await Users.update(
       {"email" : req.body.email},
       {$push : {friendList : me}}
     )
-    if(!result4.ok) return res.status(401).json({message : "ERR!"})
+    if(!result4.ok) return res.status(500).json({message : "ERR!"})
     res.status(200).json({message : "add success!"})
   })
   .post('/del', async(req,res)=>{
-
+    var result1 = await Users.update({token : req.body.token}, {$pop : {friendList : req.body.email}})
+    if(!result1.ok) return res.status(500).json({message : "ERR!"})
+    var result2 = await Users.update({email : req.body.email}, {$pop : {friendList : req.body.token}})
+    if(!result2.ok) return res.status(500).json({message : "ERR!"})
+    res.status(200).json({message : "del success!"})
   })
 }
