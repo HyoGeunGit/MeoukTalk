43f49a9df867b19df2d4f1a6bc57fe0fd2fbe335
akat32 <baduk32127@gmail.com>
2018-06-20 00:03:53 +0900

    fix DB name

--- mongo/index.js
+++ mongo/index.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-mongoose.connect('mongodb://localhost/MeoukTalkDB3');
+mongoose.connect('mongodb://localhost/TimeStoneTest1');
 
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
