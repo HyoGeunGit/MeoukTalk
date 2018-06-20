45dfcda404fcf02f9132245ac81fb25273c6f826
akat32 <baduk32127@gmail.com>
2018-06-20 11:01:15 +0900

    fix socket

--- routes/chat.js
+++ routes/chat.js
@@ -34,34 +34,28 @@ function chat(app, io, Users, Rooms, rndstring){
     if(!result) return res.status(404).json({message : "User Not Found!"})
     else return res.status(200).json({list : result.roomInvite})
   })
-  var count = 0;
-  io.on('connection', function(socket){
-    console.log('user connected: ', socket.id);
-    var name = "user" + count++;
-    io.to(socket.id).emit('change name',name);
-    socket.on('disconnect', function(){
-      console.log('user disconnected: ', socket.id);
-    });
-    socket.on('send message', function(name,text){
-      var msg = name + ' : ' + text;
-      console.log(msg);
+
+
+  io.on('connection', (socket)=>{
+    console.log('new user! : ', socket.id)
+    socket.on('join room', (name, room)=>{
+      console.log(name + ' : ' + room)
+      var text = name + "님이 방에 들어왔습니다.";
+      io.to(room).emit('welcome room', name, text);
+      socket.join(room);
+    })
+    socket.on('leave room', (name, room)=>{
+      console.log(name + ' : ' + room)
+      var text = name + "님이 방에서 나갔습니다.";
+      io.to(room).emit('goodbye room', name, text);
+      socket.leave(room);
+    })
+    socket.on('send message', (name, index, room)=>{
+      console.log(room + '. ' + name + ' : ' + index)
+      var msg = name + ' : ' + index;
       io.emit('receive message', msg);
-    });
-  });
-  // io.on('connection', (socket)=>{
-  //   socket.on('disconnect', ()=>{ console.log('user disconnect')})
-  //   socket.on('join room', (name, room)=>{
-  //     var text = name + "님이 방에 들어왔습니다.";
-  //     io.to(room).emit('welcome room', name, text);
-  //     socket.join(room);
-  //   })
-  //   socket.on('leave room', (name, room)=>{
-  //     var text = name + "님이 방에서 나갔습니다.";
-  //     io.to(room).emit('goodbye room', name, text);
-  //     socket.leave(room);
-  //   })
-  //   socket.on('send message', (name, index, room)=>{
-  //     io.to(room).emit('receive message', name, index, room);
-  //   })
-  // })
+      //io.to(room).emit('receive message', name, index, room);
+    })
+    socket.on('disconnect', ()=>{ console.log('user disconnect')})
+  })
 }
