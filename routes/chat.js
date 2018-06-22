131d483703fb39b08bfb63a28c52a8dc16fd1010
akat32 <baduk32127@gmail.com>
2018-06-22 23:46:54 +0900

    fix roomchk

--- routes/chat.js
+++ routes/chat.js
@@ -65,11 +65,14 @@ function chat(app, io, Users, Rooms, rndstring){
   })
   .post('/roomchk', async(req,res)=>{
     let result = await Users.findOne({token : req.body.token})
-    if(!result) return res.status(404).json({message : "User Not Found!"})
-    else return res.status(200).json({list : result.roomInvite})
+    for (i = 0; result.roomList[i] != null; i++)
+      if(result.roomList[i].email === req.body.email)
+        return res.status(200).send(result.roomList[i].roomID);
+    return res.status(404).json({message : "User Not Found!"})
   })
   .post('/roomList', async (req,res)=>{
     let result = await Users.findOne({token : req.body.token})
+
     if(!result) return res.status(404).json({message : "User Not Found!"})
     else return res.status(200).send(result.roomList);
   })
