a347f7811f5e975bbc767e521d022e174763542a
akat32 <baduk32127@gmail.com>
2018-06-23 03:00:53 +0900

    change /ischat return

--- routes/friend.js
+++ routes/friend.js
@@ -61,7 +61,7 @@ function friend(app, Users){
     let result = await Users.findOne({token : req.body.token})
     for (i = 0; result.friendList[i] != null; i++)
       if(result.friendList[i].email === req.body.email)
-        return res.status(200).send(result.friendList[i].isChat);
+        return res.status(200).send({"isChat" : result.friendList[i].isChat});
     return res.status(404).json({message : "User Not Found!"})
   })
 }
